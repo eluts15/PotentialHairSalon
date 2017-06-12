9cf6c0caaae601c5abbb313a7be7cbe012ace156
eluts <eluts15@gmai.com>
2017-06-11 20:48:27 -0700

    remove unnecessary comments in Client.cs

--- Objects/Client.cs
+++ Objects/Client.cs
@@ -200,10 +200,11 @@ namespace HairSalon
       SqlConnection conn = DB.Connection();
       conn.Open();
 
-      SqlCommand cmd = new SqlCommand("DELETE FROM clients WHERE id = @ClientId;", conn);
-      SqlParameter clientIdParam = new SqlParameter("@ClientId", this.GetId());
-      // clientIdParam.ParameterName = "@ClientId";
-      // clientIdParam.Value = this.GetId();
+      SqlCommand cmd = new SqlCommand("DELETE FROM clients WHERE id = @ClientId; DELETE FROM stylists WHERE id = @ClientId;", conn);
+
+      SqlParameter clientIdParam = new SqlParameter();
+      clientIdParam.ParameterName = "@ClientId";
+      clientIdParam.Value = this.GetId();
 
       cmd.Parameters.Add(clientIdParam);
       cmd.ExecuteNonQuery();
@@ -213,26 +214,6 @@ namespace HairSalon
         conn.Close();
       }
     }
-
-    // public void Delete()
-    // {
-    //   SqlConnection conn = DB.Connection();
-    //   conn.Open();
-    //
-    //   SqlCommand cmd = new SqlCommand("DELETE FROM clients WHERE id = @ClientId; DELETE FROM stylists WHERE id = @ClientId;", conn);
-    //
-    //   SqlParameter clientIdParam = new SqlParameter();
-    //   clientIdParam.ParameterName = "@ClientId";
-    //   clientIdParam.Value = this.GetId();
-    //
-    //   cmd.Parameters.Add(clientIdParam);
-    //   cmd.ExecuteNonQuery();
-    //
-    //   if(conn != null)
-    //   {
-    //     conn.Close();
-    //   }
-    // }
 
     public static void DeleteAll()
     {
